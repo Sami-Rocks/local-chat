ab55225b0cc789253e038de40fd47118dfcb91e1
Sami <sami@Sami-MacBook-Air.local>
2022-10-24 21:37:24 +0000

    input field and send button built

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors : {
+        background: '#F6F5F5',
+        primary: '#1F3C88',
+        black: "#070D2B",
+        white: "#E5E5E5",
+      },
       fontFamily: {
         sans: ["Ubuntu", "sans-serif"],
       },
